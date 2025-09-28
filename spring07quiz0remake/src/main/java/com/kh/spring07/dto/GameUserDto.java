fcb56f4c711544e40f40b08d80c49456695ce8ef
이상훈 <sanghunlee1333@gmail.com>
2025-09-28 22:35:59 +0900

    게임유저 DTO 생성 및 DB드라이버 연결 설정

---
+++ spring07quiz0remake/src/main/java/com/kh/spring07/dto/GameUserDto.java
@@ -0,0 +1,12 @@
+package com.kh.spring07.dto;
+
+import lombok.Data;
+
+@Data
+public class GameUserDto {
+	public int gameUserNo;
+	public String gameUserId;
+	public String gameUserJob;
+	public int gameUserLevel;
+	public int gameUserMoney;
+}
