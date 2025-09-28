5b72503c0f3273b1b6c32193ae2650d74b4c3cb7
이상훈 <sanghunlee1333@gmail.com>
2025-09-28 22:23:47 +0900

    프로젝트 생성

---
+++ spring07quiz0remake/src/main/java/com/kh/spring07/Spring07quiz0remakeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07quiz0remakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07quiz0remakeApplication.class, args);
+	}
+
+}
