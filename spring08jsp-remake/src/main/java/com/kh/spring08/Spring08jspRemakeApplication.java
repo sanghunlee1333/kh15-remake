d2bb7acea7963475b208789adb6a9233bf3c4fa7
이상훈 <sanghunlee1333@gmail.com>
2025-09-30 21:45:02 +0900

    프로젝트 생성

---
+++ spring08jsp-remake/src/main/java/com/kh/spring08/Spring08jspRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08jspRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08jspRemakeApplication.class, args);
+	}
+
+}
